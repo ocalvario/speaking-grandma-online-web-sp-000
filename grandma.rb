11a16a5319fa561faa38dc72c8224dc0ec673874
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-18 17:21:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,5 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma (phrase)
-  if phrase == 
+  if phrase == phrase.upcase
+    puts "NOM NOT SINCE 1938!"
+  
 end
